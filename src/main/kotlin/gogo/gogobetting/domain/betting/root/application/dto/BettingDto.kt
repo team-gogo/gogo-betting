cdc93bb6cf42ccfbd065b6bebd59c2cbcda6eb32
신희성 <vvwv3092@gmail.com>
2025-02-20 13:07:35 +0900

    add: valid

--- src/main/kotlin/gogo/gogobetting/domain/betting/root/application/dto/BettingDto.kt
+++ src/main/kotlin/gogo/gogobetting/domain/betting/root/application/dto/BettingDto.kt
@@ -1,6 +1,10 @@
 package gogo.gogobetting.domain.betting.root.application.dto
 
+import jakarta.validation.constraints.NotNull
+
 data class BettingDto(
+    @NotNull
     val predictedWinTeamId: Long,
+    @NotNull
     val bettingPoint: Long
 )
--- src/main/kotlin/gogo/gogobetting/domain/betting/root/presentation/BettingController.kt
+++ src/main/kotlin/gogo/gogobetting/domain/betting/root/presentation/BettingController.kt
@@ -2,6 +2,7 @@ package gogo.gogobetting.domain.betting.root.presentation
 
 import gogo.gogobetting.domain.betting.root.application.BettingService
 import gogo.gogobetting.domain.betting.root.application.dto.BettingDto
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -15,7 +16,7 @@ class BettingController(
     @PostMapping("/{match_id}")
     fun bet(
         @PathVariable("match_id") matchId: Long,
-        @RequestBody dto: BettingDto,
+        @RequestBody @Valid dto: BettingDto,
     ): ResponseEntity<Void> {
         bettingService.bet(matchId, dto)
         return ResponseEntity(HttpStatus.OK)
